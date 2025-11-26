8bb2d748fd969d47d69d3274d29b374edd65d13a
17TheWord <17theword@gmail.com>
2025-11-26 20:52:11 +0800

    📝 更新文档，修正快速开发和标准开发链接路径

--- docs/.vuepress/config/sidebar/development/quick-start.ts
+++ docs/.vuepress/config/sidebar/development/quick-start.ts
@@ -1,6 +1,33 @@
 export default [
     {
         text: '快速开发',
-        link: '/development/integration/quick-start/',
+        link: '/development/quick-start/',
+    },
+    {
+        text: '主类编写',
+        link: '/development/quick-start/main-class',
+    },
+    {
+        text: '事件监听',
+        link: '/development/quick-start/event-listener',
+    },
+    {
+        text: 'utils',
+        collapsible: true,
+        children: [
+            { text: '工具包', link: '/development/quick-start/utils/tool' },
+        ]
+    },
+    {
+        text: 'handle',
+        collapsible: true,
+        children: [
+            { text: 'API 处理实现', link: '/development/quick-start/handle/api' },
+            { text: '命令返回信息实现', link: '/development/quick-start/handle/command-return' },
+        ]
+    },
+    {
+        text: '命令',
+        link: '/development/quick-start/commands',
     }
 ]
